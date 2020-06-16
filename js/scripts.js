828eb4dbc9957fa3d24d3b23bc9113672d4c2d12
rujulmobilfirst <rujul.mobilefirst@gmail.com>
2020-06-16 14:23:11 +0530

    mobile header changes

--- js/scripts.js
+++ js/scripts.js
@@ -34,6 +34,19 @@
     $('.js-scroll-trigger').click(function() {
       $('.navbar-collapse').collapse('hide');
     });
+    
+    $("[data-trigger]").on("click", function(){
+        var trigger_id =  $(this).attr('data-trigger');
+        $(trigger_id).toggleClass("show");
+        $('body').toggleClass("offcanvas-active");
+    });
+    
+    // close button 
+    $(".btn-close").click(function(e){
+        $(".navbar-collapse").removeClass("show");
+        $("body").removeClass("offcanvas-active");
+    });
+
   
     // Activate scrollspy to add active class to navbar items on scroll
     // $('body').scrollspy({
